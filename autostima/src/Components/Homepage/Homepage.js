d461a3db955b2bce8e8e09d98f7d04cf3b7bf4d2
Nishtha Goyal <nishthagoyal@Nishthas-MacBook-Pro.local>
2022-02-03 20:36:59 +0530

    About Page added

--- autostima/src/Components/Homepage/Homepage.js
+++ autostima/src/Components/Homepage/Homepage.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { useEffect} from "react";
 import style from './Homepage.module.scss'
 import tree from '../Assets/tree.png'
 import landing from '../Assets/landing.png'
 import { Link } from 'react-router-dom';
+import { toast } from "react-toastify";
+
+const CustomToastWithMessage = (msg) => {
+    if (typeof msg == "string") {
+      return (
+        <div>
+          {msg}
+        </div>
+      );
+    }
+  };
+
 function Homepage() {
+    useEffect(() => {
+        fetch(
+            `https://type.fit/api/quotes`
+          )
+          .then((res) => res.json())
+          .then((res) =>{
+            const apiData = res;
+            console.log(apiData + "");
+            if(apiData.length>0){
+                toast.info(
+                    () =>
+                      CustomToastWithMessage(
+                        `Some rguvxjv hbkguftc ffuyyj abcd`
+                      ),
+                    {
+                      theme: "colored",
+                      hideProgressBar: false,
+                      closeButton: true,
+                    }
+                  );
+            }
+          });
+    });
     return <div className={style.wrapper}>
         <div className={style.left}>
             <img src={landing} />
@@ -19,3 +55,4 @@ function Homepage() {
 }
 
 export default Homepage;
+
